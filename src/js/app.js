5c85c96dd3d57fb7711ae2d9828402aca76ddd7f
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-03-12 15:10:22 -0500

    Added a splash-page for my portfolio website

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+const texts = ['developer', 'Creator', 'Designer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    //Compares Count to Text length, when it gets to 3 it resets.
+    if(count === texts.length){
+        count=0;
+    }
+    currentText = texts[count];
+    letter=currentText.slice(0, ++index);
+    //Se;ect the class and change its content to letter
+    document.querySelector('.txt-type').textContent = letter;
+    //As soon as the letter length is the same as our currentTxt, we go to the next one
+    if(letter.length === currentText.length){
+        count++;
+        //Reset starting from the beginning of the word
+        index = 0;
+    }
+
+    setTimeout(type, 400);
+})();
